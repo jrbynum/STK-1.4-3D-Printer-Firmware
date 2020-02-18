537c83160a75b8a88b19b05c97214d49532c6696
Rusty <jrbynum@shaw.ca>
2020-02-18 08:31:43 -0700

    Changed Version

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "frankienstien-1.2.2"
+#define SHORT_BUILD_VERSION "frankienstein-1.2.9"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -46,7 +46,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "Frankeinstien Printer"
+#define MACHINE_NAME "Frankeinstein Printer"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
